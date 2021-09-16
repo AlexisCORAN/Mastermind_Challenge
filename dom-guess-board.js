36fc61bde4e10191b18430b9481058b2212d540e
rennleon <2829@holbertonschool.com>
2021-09-16 21:29:10 +0000

    Refactor code and files structure

---
+++ dom-guess-board.js
@@ -0,0 +1,49 @@
+function createGuessButtons() {
+    const guessRowHTML = (guessPeg, gradePeg) => `
+        <div class="guess-row">
+            <div class="guess-pegs">
+                ${guessPeg}
+            </div>
+            <div class="grade-pegs">
+                ${gradePeg}
+            </div>
+        </div>
+    `
+
+    const [rows, cols] = [10, 4]
+    for (let row = 0; row < rows; row++)
+    {
+        let guessHTML = ''
+        let gradeHTML = ''
+        for (let col = 0; col < cols; col++)
+        {
+            guessHTML += `
+                <div id="guess-${row}-${col}" class="guess-peg"><button>peg${row}${col}</button></div>
+            `
+            gradeHTML += `
+                <div id="grade-${row}-${col}" class="grade-peg"><button>grade${row}${col}</button></div>
+            `
+        }
+
+        DOM.guessContainer.insertAdjacentHTML('beforeend', guessRowHTML(guessHTML, gradeHTML))
+    }
+}
+
+function addListenersToGuessButtons() {
+    document.querySelectorAll('.guess-peg')
+        .forEach(el => el.addEventListener('click', () => {
+                console.log(el.id)
+            })
+        )
+
+    document.querySelectorAll('.grade-peg')
+        .forEach(el => el.addEventListener('click', () => {
+                console.log(el.id)
+            })
+        )
+}
+
+function renderGuessButtons() {
+    createGuessButtons()
+    addListenersToGuessButtons()
+}
