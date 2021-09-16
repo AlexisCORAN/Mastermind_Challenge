ce2e0bbc4fe43e3b0b0f5b713c3c1439c3a58d79
rennleon <2829@holbertonschool.com>
2021-09-16 04:28:29 +0000

    Add listeners

--- index.js
+++ index.js
@@ -22,12 +22,19 @@ for (let row = 0; row < rows; row++)
     for (let col = 0; col < cols; col++)
     {
         guessHTML += `
-            <div id="guess-${row}-${col}" class="guess-peg"></div>
+            <div id="guess-${row}-${col}" class="guess-peg"><button>This is peg${row}${col}</button></div>
         `
         gradeHTML += `
-            <div id="grade-${row}-${col}" class="grade-peg"></div>
+            <div id="grade-${row}-${col}" class="grade-peg"><button>This GRADE${row}${col}</button></div>
         `
     }
 
     DOM.guessContainer.insertAdjacentHTML('beforeend', guessRowHTML(guessHTML, gradeHTML))
 }
+
+const guessPegs = document.querySelectorAll('.guess-peg')
+const gradePegs = document.querySelectorAll('.grade-peg')
+
+guessPegs.forEach(el => el.addEventListener('click', () => console.log(el.id)))
+
+gradePegs.forEach(el => el.addEventListener('click', () => console.log(el.id)))
