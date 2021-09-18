da8f657725d68adc744949eb5d20653a490d806c
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-18 04:11:02 +0000

    Deleted files

--- Game_Files/dom.js
+++
@@ -1,6 +0,0 @@
-const DOM = {
-    answerContainer: document.getElementById('answerContainer'),
-    guessContainer: document.getElementById('guessContainer'),
-    selectionContainer: document.getElementById('selectionContainer')
-}
-
