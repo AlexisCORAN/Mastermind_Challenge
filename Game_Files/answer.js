f7262ee7decdc7552082741927f17494f448427f
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-18 04:00:08 +0000

    Game files in a directory

---
+++ Game_Files/answer.js
@@ -0,0 +1,28 @@
+function createRandomAnswer() {
+    answer = []
+    let allColors = Object.keys(colors)
+    let ind, size = allColors.length
+    for (let i = 0; i < cols; i++)
+    {
+        ind = Math.floor(Math.random() * size);
+        answer.push(allColors[ind])
+    }
+}
+
+function renderAnswerHidden() {
+    let answerHTML = answer.reduce((acc, _) => `${acc}<div class="answer-peg">?</div>`, '')
+
+    DOM.answerContainer.insertAdjacentHTML('beforeend', answerHTML)
+}
+
+function renderAnswer() {
+    let answerHTML = answer.reduce((acc, color) => `${acc}<div style="background: ${colors[color]};" class="answer-peg"></div>`, '')
+
+    DOM.answerContainer.innerHTML = ''
+    DOM.answerContainer.insertAdjacentHTML('beforeend', answerHTML)
+}
+
+function initAnswer() {
+    createRandomAnswer()
+    renderAnswerHidden()
+}
